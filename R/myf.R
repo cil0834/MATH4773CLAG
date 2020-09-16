c48daf9803c4773ddc488e56c51dc9672edccab0
Caleb Lagge <caleb.i.lagge-1@ou.edu>
2020-09-16 11:04:40 -0500

    First commit

---
+++ R/myf.R
@@ -0,0 +1,14 @@
+#' @title A start for a function
+#'
+#' @description Starter function for MATH4773CLAG
+#'
+#' @param x a vector of data
+#'
+#' @return a vector whose components are squared
+#' @export
+#'
+#' @examples
+#' myf(1:10)
+myf = function(x){
+  x^2
+}
